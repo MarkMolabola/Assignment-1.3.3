b25702d3c2fad600ad0c98efec57c0c7a8d4e918
MarkMolabola <mark.molabola@outlook.com>
2025-07-09 13:00:23 -0700

    update1

--- Program.cs
+++ Program.cs
@@ -9,32 +9,27 @@ namespace Assignment_1._3._3
     {
         static void Main(string[] args)
         {
-            //Write a program in C# to read n number of values in an array and display it in reverse order.
-            //Test Data :
-            //Input the number of elements to store in the array :3
-            //Input 3 number of elements in the array :
-            //element - 0 : 2
-            //element - 1 : 5
-            //element - 2 : 7
-            //Expected Output:
-            //The values store into the array are:
-            //2 5 7
-            //The values store into the array in reverse are :
-            //7 5 2
+            Console.WriteLine("Input the number of elements to store in the array :");
+            int numberOfElements = Convert.ToInt32(Console.ReadLine());
+            int[] numberArray = new int[numberOfElements];
+            Console.WriteLine("Input " + numberOfElements + " numbers in the array :");
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.Write("number - " + (i+1) + " : ");
+                numberArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-            Console.WriteLine("Hello, World!");
-            int[] numberArray = { 2, 5, 7 };
             Console.WriteLine("The values store into the array are:");
             for (int i = 0; i < numberArray.Length; i++)
             {
-                Console.Write("element - " + i + " : " + numberArray[i] + "\n");
+                Console.Write("index - " + i + " : " + numberArray[i] + "\n");
             }
             Console.WriteLine();
             Console.WriteLine("The values store into the array in reverse are :");
-        
-            for (int i = numberArray.Length-1, j = 0; j < numberArray.Length; i--, j++)
+
+            for (int i = numberArray.Length - 1, j = 0; j < numberArray.Length; i--, j++)
             {
-                Console.Write("element - " + j + " : " + numberArray[i] + "\n");
+                Console.Write("index - " + j + " : " + numberArray[i] + "\n");
             }
         }
     }
